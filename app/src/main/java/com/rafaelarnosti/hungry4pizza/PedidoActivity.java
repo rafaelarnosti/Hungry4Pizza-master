6b35477efdf7d1a298cb6a6c38994ed361764df5
Rafael Arnosti <rlm.arnosti@gmail.com>
2017-06-05 20:20:25 -0300

    Commit

--- app/src/main/java/com/rafaelarnosti/hungry4pizza/PedidoActivity.java
+++ app/src/main/java/com/rafaelarnosti/hungry4pizza/PedidoActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
@@ -45,6 +47,22 @@ public class PedidoActivity extends AppCompatActivity {
         String usuario = bundle.getString("usuario");
 
         tvUsuario.setText("Olá " + usuario);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Item1",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void calcular(View view) {
