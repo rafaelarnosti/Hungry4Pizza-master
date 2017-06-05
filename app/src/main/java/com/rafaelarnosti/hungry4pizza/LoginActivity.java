0998955f3edcfd0c093d05a62295440f70880cc3
Rafael Arnosti <rlm.arnosti@gmail.com>
2017-06-05 19:39:33 -0300

    Commit

--- app/src/main/java/com/rafaelarnosti/hungry4pizza/LoginActivity.java
+++ app/src/main/java/com/rafaelarnosti/hungry4pizza/LoginActivity.java
@@ -4,18 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText tilLogin;
+    private EditText tilSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tilLogin = (EditText) findViewById(R.id.tilLogin);
+        tilSenha = (EditText) findViewById(R.id.tilSenha);
+
     }
     public void logar(View v){
-            Intent intent = new Intent(LoginActivity.this,
-                    PedidoActivity.class);
+
+        String Login = tilLogin.getText().toString();
+        String Senha = tilSenha.getText().toString();
+
+        if(Login.equals("FIAP") && Senha.equals("123")){
+            Intent intent = new Intent(this, PedidoActivity.class);
+            intent.putExtra("usuario",Login);
             startActivity(intent);
-        LoginActivity.this.finish();
+
+        }else{
+            Toast.makeText(this,"Usuario ou senhas invalidos",Toast.LENGTH_SHORT).show();
+        }
     }
 }
